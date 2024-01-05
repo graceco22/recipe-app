3882ddc4eda44b02289e8c5eced083191af731dd
graceco22 <132722611+graceco22@users.noreply.github.com>
2024-01-05 01:21:11 -0500

    title update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -78,7 +78,7 @@ const App = () => {
     <div className="app-container">
       <div className="header">
         <img src="/food3.jpeg"></img>
-        <div className="title">Grace's Recipe App</div>
+        <div className="title">Yummy Source</div>
       </div>
       <div className="tabs">
         <h1
